d1e43c2ae4c5f32845471dba19999082f7ce8b4c
Ilya Petrosyan <im.clean@yandex.ru>
2022-04-25 11:17:24 +0300

    [TurboModule] Update the Native iOS code

--- ios/RNCalculator/RNCalculator.h
+++ ios/RNCalculator/RNCalculator.h
@@ -7,6 +7,20 @@
 
 #import <React/RCTBridgeModule.h>
 
+// Thanks to this guard, we won't import this header when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNCalculatorSpec.h"
+#endif
+
 @interface RNCalculator : NSObject <RCTBridgeModule>
+
+// Thanks to this guard, we won't compile this code when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeCalculatorSpecJSI>(params);
+}
+#endif
 
 @end
