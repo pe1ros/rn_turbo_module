aa23aa10a124381bd63596b9494b7011f33f0db7
Ilya Petrosyan <im.clean@yandex.ru>
2022-04-25 11:00:05 +0300

    [TurboModule] Add the JavaScript specs

---
+++ src/NativeCalculator.js
@@ -0,0 +1,25 @@
+// @flow
+import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  // your module methods go here, for example:
+  add(a: number, b: number): Promise<number>;
+}
+
+export default (TurboModuleRegistry.get<Spec>(
+'Calculator'
+): ?Spec);
+
+
+// For TS it will be:
+
+// import type { TurboModule } from 'react-native';
+// import { TurboModuleRegistry } from 'react-native';
+
+// export interface Spec extends TurboModule {
+  // your module methods go here, for example:
+  // add(a: number, b: number): Promise<number>;
+// }
+// 
+// export default TurboModuleRegistry.get<Spec>('Calculator');
