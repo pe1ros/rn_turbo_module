78272f1b24cf0f5fc1086a566014390d1840b563
Ilya Petrosyan <im.clean@yandex.ru>
2022-04-25 10:08:42 +0300

    Create new Static Library inside ios folder

---
+++ ios/RNCalculator/RNCalculator.h
@@ -0,0 +1,12 @@
+//
+//  RNCalculator.h
+//  RNCalculator
+//
+//  Created by Ilya Petrosyan on 25.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCalculator : NSObject
+
+@end
---
+++ ios/RNCalculator/RNCalculator.m
@@ -0,0 +1,12 @@
+//
+//  RNCalculator.m
+//  RNCalculator
+//
+//  Created by Ilya Petrosyan on 25.04.2022.
+//
+
+#import "RNCalculator.h"
+
+@implementation RNCalculator
+
+@end
